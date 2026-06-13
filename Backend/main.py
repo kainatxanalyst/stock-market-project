b3dfb260359ea9acb7f1a491322b69841a1cc64c
kainatxanalyst <kainatarshad996@gmail.com>
2026-06-13 17:27:19 +0500

    Update main.py

--- Backend/main.py
+++ Backend/main.py
@@ -101,4 +101,4 @@ def predict(data: StockInput):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="0.0.0.0", port=10000)

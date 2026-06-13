f14934388ac0230f1ae9b9b74fceaa959a453787
kainatxanalyst <kainatarshad996@gmail.com>
2026-06-13 23:09:42 +0500

    fix: backend + dependencies updated for render

--- Backend/main.py
+++ Backend/main.py
@@ -115,5 +115,8 @@ def predict(data: StockInput):
 
 
 if __name__ == "__main__":
+    import os
+    import uvicorn
+
     port = int(os.environ.get("PORT", 8000))
     uvicorn.run(app, host="0.0.0.0", port=port)
